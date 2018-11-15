0f90491e30ad457d91ae07c994be1f088d21e1cd
jforjohn <giannisotiro@gmail.com>
2018-11-15 19:13:45 +0100

    PCA: change way it sorts

--- MyPCA.py
+++ MyPCA.py
@@ -23,7 +23,6 @@ class MyPCA:
 
         # Step4: Compute the covariance matrix of the whole data set.
         #cov_mat = np.cov(data.T)
-        #print(cov_mat)
         self.cov_mat = np.matmul(data_adjust.T, data_adjust) / (data_adjust.shape[0]-1)
 
         # Step 5. Calculate eigenvectors (e1, e2, …, ed) and their corresponding eigenvalues of the
@@ -32,13 +31,15 @@ class MyPCA:
 
         # Step 6. Sort the eigenvectors by decreasing eigenvalues and choose k eigenvectors with the
         # largest eigenvalues to form a new d x k dimensional matrix.
-        choose_n =  sorted(zip(self.eigval, self.eigvec.T), reverse=True)[:self.n_components]
+        choose_n =  sorted(zip(self.eigval, self.eigvec.T),
+            key=lambda x: x[0],
+            reverse=True)[:self.n_components]
         self.n_eigval = np.array(list(map(lambda x: x[0], choose_n)))
         self.n_eigvec = np.array(list(map(lambda x: x[1], choose_n))).T
         
         # Step 7. Derive the new data set. Use this d x k eigenvector matrix to transform the samples
         # onto the new subspace.
-        self.transformedData = np.matmul(self.n_eigvec.T, data_adjust.T).T
+        self.transformedData = np.matmul(data_adjust, self.n_eigvec)
 
         # Step 9. Reconstruct the data set back to the original one
         self.data_new = np.matmul(self.transformedData, self.n_eigvec.T) + mean_vector
@@ -52,4 +53,6 @@ data = np.array([[2,3,8],
                  [12,10,10]])
 clf = MyPCA(n_components=2)
 clf.fit(data)
+print(clf.n_eigval)
+print(clf.eigval)
 '''

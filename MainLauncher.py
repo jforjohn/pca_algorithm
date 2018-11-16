238681a0369b5f16069df44c3ec5e37386f236f3
jforjohn <giannisotiro@gmail.com>
2018-11-16 17:30:47 +0100

    MAINLAUNCHER: remove for loop and fix plots

--- MainLauncher.py
+++ MainLauncher.py
@@ -22,8 +22,24 @@ import matplotlib.pyplot as plt
 from Validation import validation_metrics
 from Validation import best_k
 
+def kmeans_comparison(k,tol,max_rep, data, name):
+    start = time()
+    clf = MyKmeans(k, tol, max_rep)
+    clf.fit(data)
+    duration = time() - start
+    metrics = validation_metrics(df, labels, clf.labels_)
+    max_rep = 100 - clf.max_rep
+
+    metrics.update({"Time": duration,
+                    "Repetitions": max_rep})
+    validations = pd.DataFrame.from_dict(metrics, orient='index',
+                                            columns=[clf.name+name])
+    return validations
+
+
 ##
 if __name__ == '__main__':
+    ##
     # Loads config
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -45,9 +61,12 @@ if __name__ == '__main__':
         sys.exit(1)
 
     ##
-    kmeans_init_type = config.get('clustering', 'kmeans_init_type')
-    n_components = config.get('pca', 'n_components').split('-')
+    #kmeans_init_type = config.get('clustering', 'kmeans_init_type')
+    n_components = int(config.get('pca', 'n_components')) #.split('-')
     num_plot_features = int(config.get('pca', 'num_plot_features'))
+    k = int(config.get('clustering', 'k'))
+    tol = float(config.get('clustering', 'tol'))
+    max_rep = int(config.get('clustering', 'max_rep'))
 
     ## Preprocessing
     preprocess = MyPreprocessing()
@@ -55,49 +74,70 @@ if __name__ == '__main__':
     df = preprocess.new_df
     labels = preprocess.labels_
 
-    for n_component in n_components:
-        # PCA
-        n_component = int(n_component)
-        pca = MyPCA(n_components=n_component)
-        pca.fit(df)
+    #for n_component in n_components:
 
-        if hasattr(pca, 'cov_mat'):
-            print('Original Covariance Matrix: ')
-            print(pca.cov_mat)
-            print()
+    # PCA
 
-        if hasattr(pca, 'eigval') and hasattr(pca, 'eigvec'):
-            print('Original eigen values and corresponding eigen vectors: ')
-            print(pca.eigval)
-            print(pca.eigvec)
-            print()
+    ## MyPCA
+    mypca = MyPCA(n_components=n_components)
+    mypca.fit(df)
 
-        print('K max eigen values and corresponding eigen vectors: ')
-        print(pca.explained_variance_)
-        print(pca.components_)
-        print()
+    print('Original Covariance Matrix: ')
+    print(mypca.cov_mat)
+    print()
 
-        print('Explained variance for %d components: ' %(n_component))
-        print(np.cumsum(pca.explained_variance_ratio_))
-        print()
+    print('Original eigen values and corresponding eigen vectors: ')
+    print(mypca.eigval)
+    print(mypca.eigvec)
+    print()
 
-        #specialPairPlot(pca.tranformedData)
+    print('K max eigen values and corresponding eigen vectors: ')
+    print(mypca.explained_variance_)
+    print(mypca.components_)
+    print()
 
-        ## Sklearn PCA
-        print('PCA sklearn algorithm ')
-        pca = PCA(n_components=n_component)
-        pca.fit_transform(df)
-        print(pca.explained_variance_)
-        print(np.cumsum(pca.explained_variance_ratio_))
-        print()
+    print('Explained variance for %d components: ' %(n_components))
+    print(np.cumsum(mypca.explained_variance_ratio_))
+    print()
 
-        print('IncrementalPCA sklearn  algorithm ')
-        pca = IncrementalPCA(n_components=n_component)
-        pca.fit_transform(df)
-        print(pca.explained_variance_)
-        print(np.cumsum(pca.explained_variance_ratio_))
-        print()
+    #specialPairPlot(pca.tranformedData)
 
-        Pair_Plot(df, num_plot_features)
+    ## Sklearn PCA
+    print('PCA sklearn algorithm ')
+    pca_skl = PCA(n_components=n_components)
+    pca_skl.fit_transform(df)
+    print(pca_skl.explained_variance_)
+    print(np.cumsum(pca_skl.explained_variance_ratio_))
+    print()
 
+    print('IncrementalPCA sklearn  algorithm ')
+    ipca_skl = IncrementalPCA(n_components=n_components)
+    ipca_skl.fit_transform(df)
+    print(ipca_skl.explained_variance_)
+    print(np.cumsum(ipca_skl.explained_variance_ratio_))
+    print()
 
+    Pair_Plot(df, num_plot_features)
+    
+    diff_explained = list(map(np.cumsum, [mypca.explained_variance_,pca_skl.explained_variance_,ipca_skl.explained_variance_]))
+    df_plot = pd.DataFrame(np.array(
+        diff_explained).T,
+        columns=['mypca','pca_skl', 'ipca_skl'])
+    df_plot.plot.bar(rot=0)
+    
+
+    
+    ## Kmeans comparison
+    validations_nonpca = kmeans_comparison(k, tol, max_rep, df, '')
+    validations_pca = kmeans_comparison(k, tol, max_rep, mypca.transformedData, 'PCA')
+    
+    validations = pd.concat([validations_nonpca, validations_pca], axis=1)
+    print(validations)
+
+    # Show the percentage of repetitions
+    validations.iloc[-1, :] = validations.iloc[-1, :]/100.0
+
+    #validations = validations.drop(validations.index[len(validations.index) - 1], axis=0)
+    validations.plot.bar(rot=30)
+    plt.subplots_adjust(bottom=0.2)
+    plt.show()
